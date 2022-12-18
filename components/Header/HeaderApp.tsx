386a0eac56fa808f0c29240580d2453579062a38
Nikolyovin <pulya0763@gmail.com>
2022-12-18 22:17:46 +0300

    first commit

---
+++ components/Header/HeaderApp.tsx
@@ -0,0 +1,13 @@
+import { Menu, Layout } from 'antd'
+// import { Header } from 'antd/es/layout/layout'
+import React from 'react'
+
+const HeaderApp = () => {
+	return (
+		<Layout.Header>
+			<h1>Список желаний</h1>
+		</Layout.Header>
+	)
+}
+
+export default HeaderApp
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from 'antd'
+import { FC } from 'react'
+import HeaderApp from '../components/Header/HeaderApp'
+
+const Home: FC = () => {
+	return (
+		<Layout className="layout">
+			<HeaderApp />
+		</Layout>
+	)
+}
+
+export default Home
